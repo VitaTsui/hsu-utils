38594442f15470284eff0af1f749d51646c870f9
VitaHsu <vitahsu7@gmail.com>
2024-02-24 02:54:44 +0800

    modify get_string_width

--- tools/GetStrWidth/__tests__/GetStrWidth.test.ts
+++ tools/GetStrWidth/__tests__/GetStrWidth.test.ts
@@ -1,8 +1,14 @@
 import { describe, expect, test } from '@jest/globals'
 import { get_string_width } from '../..'
+import 'jest-canvas-mock'
 
 describe('get_string_width', () => {
-  test(`get_string_width('测试-test-1') = 5.5`, () => {
-    expect(get_string_width('测试-test-1') === 5.5).toBe(true)
+  /**
+   * TODO：canvas 包安装过于繁琐与困难，暂不进行此项单元测试
+   */
+  test(`get_string_width('测试-test-1') = 61.92`, () => {
+    const width = get_string_width('测试-test-1')
+
+    expect(width === 61.92).toBe(true)
   })
 })
