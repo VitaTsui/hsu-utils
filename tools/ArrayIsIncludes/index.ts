f1e51e06c1ba4309f0c7b38d44c325d5f20592f1
VitaHsu <vitahsu7@gmail.com>
2024-06-16 00:52:20 +0800

    modify array_is_includes

--- tools/ArrayIsIncludes/index.ts
+++ tools/ArrayIsIncludes/index.ts
@@ -1,4 +1,4 @@
-import { Equal } from '../../lib'
+import { Equal } from '../..'
 
 export default function array_is_includes<T>(arr1: Array<T>, arr2: Array<T>) {
   const largeArr = arr1.length >= arr2.length ? arr1 : arr2
