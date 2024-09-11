4ff9a9d60c7c83be7f305d923c1f4244d40e0acf
vitahsu <vitahsu7@gmail.com>
2024-09-11 17:42:49 +0800

    修改 array_is_includes

--- tools/ArrayIsIncludes/index.ts
+++ tools/ArrayIsIncludes/index.ts
@@ -1,8 +1,8 @@
 import { Equal } from '..'
 
 export default function array_is_includes<T>(arr1: Array<T>, arr2: Array<T>) {
-  const largeArr = arr1.length >= arr2.length ? arr1 : arr2
-  const smallArr = arr1.length < arr2.length ? arr1 : arr2
+  const smallArr = arr1.length <= arr2.length ? arr1 : arr2
+  const largeArr = arr1.length > arr2.length ? arr1 : arr2
 
   return smallArr.every((item) => {
     return !!largeArr.find((i) => Equal.ValEqual(item, i))
