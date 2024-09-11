2bfd99620d8a7aae93815394df9211089edd6b3e
vitahsu <vitahsu7@gmail.com>
2024-09-11 18:47:00 +0800

    修改 array_is_includes

--- tools/ArrayIsIncludes/index.ts
+++ tools/ArrayIsIncludes/index.ts
@@ -1,10 +1,29 @@
 import { Equal } from '..'
 
+function countMap<T>(arr: Array<T>) {
+  const map = new Map()
+
+  arr.forEach((item) => {
+    map.set(item, (map.get(item) || 0) + 1)
+  })
+
+  return map
+}
+
 export default function array_is_includes<T>(arr1: Array<T>, arr2: Array<T>) {
   const smallArr = arr1.length <= arr2.length ? arr1 : arr2
   const largeArr = arr1.length > arr2.length ? arr1 : arr2
 
-  return smallArr.every((item) => {
-    return !!largeArr.find((i) => Equal.ValEqual(item, i))
-  })
+  const smallArrMap = countMap(smallArr)
+  const largeArrMap = countMap(largeArr)
+
+  for (const [key, count] of smallArrMap.entries()) {
+    const largekey = [...largeArrMap.keys()].find((item) => Equal.ValEqual(item, key))
+
+    if (!largekey || (largekey && largeArrMap.get(largekey) !== count)) {
+      return false
+    }
+  }
+
+  return true
 }
